1979f940df76cf3411f7572e4e7aad41d2330bd6
cwrong <sjy0175@gmail.com>
2023-10-18 23:19:34 +0900

    PlaceCard, detail needed(bug fix)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
 import PlaceCard from "@/components/common/PlaceCard";
 
 export default function HomePage() {
-  const numbers = Array.from({ length: 1000 }, (_, index) => index + 1);
+  const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
 
   return (
     <section className={"text-h1 bg-bg"}>
