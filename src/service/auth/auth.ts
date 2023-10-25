5c9360b98ab8bdc15828e59d5be0810d2bcb09be
cwrong <sjy0175@gmail.com>
2023-10-25 16:35:26 +0900

    add User

--- src/service/auth/auth.ts
+++ src/service/auth/auth.ts
@@ -1,19 +1,42 @@
-import { CredentialUserHashed, SanityUser, userType } from "@/model/user";
+import {
+  CredentialUserHashed,
+  providerType,
+  SanityUser,
+  User,
+  UserRequest,
+  userType,
+} from "@/model/user";
 import { sanityClient } from "@/service/sanity";
 import { getUserNumber } from "@/service/user/user";
+
+export const addOAuthUser = async (user: UserRequest): Promise<boolean> => {
+  const cntUser = await getUserNumber();
+  const newUser: SanityUser = {
+    _id: `user-${user.provider}-${user.username}`,
+    _type: userType,
+    id: cntUser + 1,
+    ...user,
+  };
+
+  const res = await sanityClient.createIfNotExists(newUser);
+  return !!res;
+};
 
 export const addCredentialUser = async (
   user: CredentialUserHashed,
 ): Promise<boolean> => {
-  if (!(await isNewUsername(user.username))) return false;
+  const { username, pwHash } = user;
+
+  if (!(await isNewUsername(username))) return false;
 
   const cntUser = await getUserNumber();
   const newUser: SanityUser = {
-    _id: `user.${cntUser + 1}`,
-    _type: "user",
+    _id: `user-${providerType.credential}.${username}`,
+    _type: userType,
     id: cntUser + 1,
-    ...user,
-    provider: "credential",
+    username,
+    pwHash,
+    provider: providerType.credential,
   };
 
   const res = await sanityClient.create(newUser);
@@ -21,15 +44,19 @@ export const addCredentialUser = async (
 };
 
 export const isUniqueUsername = async (username: string): Promise<boolean> => {
-  const cnt = await sanityClient.fetch<number>(
-    `count(*[_type == "${userType}" && username == "${username}"])`,
-  );
+  const cnt = await getCredentialUserNumberByUsername(username);
   return cnt === 1;
 };
 
 export const isNewUsername = async (username: string): Promise<boolean> => {
-  const cnt = await sanityClient.fetch<number>(
-    `count(*[_type == "${userType}" && username == "${username}"])`,
-  );
+  const cnt = await getCredentialUserNumberByUsername(username);
   return cnt === 0;
+};
+
+export const getCredentialUserNumberByUsername = async (
+  username: string,
+): Promise<number> => {
+  return await sanityClient.fetch<number>(
+    `count(*[_type == "${userType}" && provider == "${providerType.credential}"  && username == "${username}"])`,
+  );
 };
